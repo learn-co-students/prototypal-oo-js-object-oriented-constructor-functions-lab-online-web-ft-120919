33d683d57bfab5c6fb086e6b5a886eed73352254
adrian mohabir <mohabiradrian@gmail.com>
2021-04-20 17:23:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-function Scooter(year, color, model) {
-    this.year = year
-    this.color = color
-    this.model = model
+class Scooter {
+    constructor(year, color, model) {
+        this.year = year
+        this.color = color
+        this.model = model
+    }
 }
 
-function Driver(name, age, experience) {
-    this.name = name
-    this.age = age
-    this.experience = experience
+class Driver {
+    constructor(name, age, experience) {
+        this.name = name
+        this.age = age
+        this.experience = experience
+    }
 }
 
-function PickupLocation(address, city) {
-    this.address = address
-    this.city = city
+class PickupLocation {
+    constructor(address, city) {
+        this.address = address
+        this.city = city
+    }
 }
